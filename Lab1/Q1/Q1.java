6e4153d8607cbc3bb75c210d0891b29ad257cd4a
leezhenxue <zhenxue2880@gmail.com>
2025-03-21 16:03:20 +0800

    Q1 part 2 done

--- Lab1/Q1/Q1.java
+++ Lab1/Q1/Q1.java
@@ -1,11 +1,34 @@
 package Q1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 public class Q1 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String lines;
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("Lab1/Q1/LeeZhenXue_24004491.txt", true));
+            while(true){
+                lines = input.nextLine();
+                if (lines.isEmpty()){
+                    break;
+                }
+                writer.write(lines);
+                writer.newLine();
+            }
+            writer.newLine();
+            writer.close();
+        } catch (FileNotFoundException e){
+            e.getMessage();
+        } catch (IOException e){
+            e.getMessage();
+        }
+        
         try{
             BufferedReader reader = new BufferedReader(new FileReader("Lab1/Q1/LeeZhenXue_24004491.txt"));
             String line;
