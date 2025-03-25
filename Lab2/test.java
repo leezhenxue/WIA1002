b7afbba160e7648c71158240d46661f192dfe06e
leezhenxue <zhenxue2880@gmail.com>
2025-03-25 15:42:57 +0800

    done Tutorial 2

--- Lab2/test.java
+++ Lab2/test.java
@@ -1,18 +1,3 @@
 package Lab2;
 
-class Duo<A, B> {
-    private A first;
-    private B second;
 
-    public Duo(A first, B second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
-class Q6 {
-    public static void main(String[] args) {
-        Duo<String, Integer> sideShape = new Duo<>("Hello", 123);
-        Duo<Double, Double> points = new Duo<>(1.2, 12.3);
-    }
-}
