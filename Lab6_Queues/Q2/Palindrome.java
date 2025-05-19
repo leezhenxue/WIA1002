f3f0f5e347973bfbd1d2c0829e6b26979744343a
leezhenxue <zhenxue2880@gmail.com>
2025-05-19 09:56:21 +0800

    -

--- Lab6_Queues/Q2/Palindrome.java
+++ Lab6_Queues/Q2/Palindrome.java
@@ -1,23 +1,27 @@
+
 import java.util.Scanner;
 
 public class Palindrome {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String word = input.next();
-        Queue queue = new Queue<String>(word);
-        Queue queue2 = new Queue<String>(new StringBuilder(word).reverse().toString());
-        String first = null, second = null;
-        boolean palindrome = true;
-        for (int i = 0; i < queue.getSize(); i++) {
-            if (!queue.dequeue().equals(queue2.dequeue())) {
-                System.out.println("The word " + word + " is not palindrome.");
-                palindrome = false;
-                break;
-            }
+        String word = input.next().toLowerCase();
+        Queue<Character> queue = new Queue<>();
+        for (int i = word.length() - 1; i >= 0; i--) {
+            queue.enqueue(word.toCharArray()[i]);
         }
 
-        if (palindrome) {
-            System.out.println("The word " + word + " is palindrome.");
+        String reversed = "";
+        while (!queue.isEmpty()) {
+            reversed += queue.dequeue();
         }
+
+        if (word.equals(reversed)) {
+            System.out.println("The word " + word + " is palindrome.");
+        } else {
+            System.out.println("The word " + word + " is not palindrome.");
+
+        }
+
     }
 }
