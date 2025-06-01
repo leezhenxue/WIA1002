d219832dfcf7643b210d99fd3668aa812fec4a12
leezhenxue <zhenxue2880@gmail.com>
2025-06-02 02:03:56 +0800

    -

--- LabTest1/Geometric.java
+++
@@ -1,4 +0,0 @@
-public interface Geometric {
-    public double calculateVolume();
-    public double calculateSurfaceArea();
-}
--- wia1002/src/main/java/Lab3_ADT_and_Bag/Ans/PiggyBank/CoinName.java
+++
@@ -1,2 +0,0 @@
-package Lab3_ADT_and_Bag.Ans.PiggyBank;
-public enum CoinName {PENNY, NICKEL, DIME, QUARTER, FIFTY_CENT, DOLLAR}
